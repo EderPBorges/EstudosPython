b10a94c5fb0770c270904799bc732096612595e0
Eder Borges <ederborgesads@gmail.com>
2022-01-16 17:38:01 -0300

    Funçao de alta ordem

--- programacao_funcional/funcao_alta_ordem.py
+++ programacao_funcional/funcao_alta_ordem.py
@@ -10,4 +10,4 @@ def processar(titulo, lista, funcao):
 
 if __name__ == '__main__':
     processar('Dobros de 1 a 10', range(1, 11), dobro)
-    processar('Wuadrados de 1 a 10', range(1, 11), quadrado)
+    processar('Quadrados de 1 a 10', range(1, 11), quadrado)
