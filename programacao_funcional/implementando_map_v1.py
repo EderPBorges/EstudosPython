f63a2f48370c678834bf1ef615716aea9123536e
Eder Borges <ederborgesads@gmail.com>
2022-02-03 13:52:03 -0300

    usando map + generator

---
+++ programacao_funcional/implementando_map_v1.py
@@ -0,0 +1,9 @@
+#!python3
+# Impleentação simplificada do map
+def mapear(funcao, lista):
+    for elemento in lista:
+        yield funcao(elemento)
+
+
+if __name__ == "__main__":
+    print(list(mapear(lambda x: x ** 2, [2, 3, 4])))
